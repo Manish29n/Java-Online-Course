7c4fcc7ac983b08571e00f28bf8756633c5adfc7
Manish N <manish29na@gmail.com>
2023-03-26 19:10:17 +0530

    Add files via upload

---
+++ Coding Exercise/CE-6EqualSumChecker/src/EqualSumChecker.java
@@ -0,0 +1,6 @@
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int one, int two, int three) {
+        int sum = one + two;
+        return sum == three;
+    }
+}
---
+++ Coding Exercise/CE-6EqualSumChecker/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean Result;
+        System.out.println(Result = EqualSumChecker.hasEqualSum(1, 2, 3));
+        System.out.println(Result = EqualSumChecker.hasEqualSum(3, 3, 6));
+        System.out.println(Result = EqualSumChecker.hasEqualSum(1, 4, 3));
+    }
+}
