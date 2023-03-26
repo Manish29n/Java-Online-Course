ddd10dd45686cc3792d0d64691f43135d546c5aa
Manish N <manish29na@gmail.com>
2023-03-26 19:09:18 +0530

    Add files via upload

---
+++ Coding Exercise/CE-4LeapYearCalculator/src/Check.java
@@ -0,0 +1,9 @@
+public class Check {
+    public static boolean isLeapYear(int year) {
+        if (year >= 1 && year <= 9999) {
+            return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0 && year % 100 == 0);
+        } else {
+            return false;
+        }
+    }
+}
---
+++ Coding Exercise/CE-4LeapYearCalculator/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean Leap;
+        System.out.println(Leap=LeapYearCalculator.isLeapYear(2000));
+        System.out.println(Leap=LeapYearCalculator.isLeapYear(2020));
+        System.out.println(Leap=LeapYearCalculator.isLeapYear(2100));
+        System.out.println(Leap=LeapYearCalculator.isLeapYear(10000));
+        System.out.println(Leap=Check.isLeapYear(2000));
+        System.out.println(Leap=Check.isLeapYear(2020));
+        System.out.println(Leap=Check.isLeapYear(2100));
+        System.out.println(Leap=Check.isLeapYear(10000));
+    }
+}
