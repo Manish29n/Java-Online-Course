15fc4f52d8b282d7beda844f689729c160bdf07e
Manish N <manish29na@gmail.com>
2023-03-28 20:48:18 +0530

    Add files via upload

---
+++ Coding Exercise/CE-12NumberInWord/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        NumberInWord.printNumberInWord(2);
+        NumberInWord.printNumberInWord(7);
+        NumberInWord.printNumberInWord(9);
+        NumberInWord.printNumberInWord(-1);
+        NumberInWord.printNumberInWord(0);
+        NumberInWord.printNumberInWord(10);
+    }
+}
