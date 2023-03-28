6a6602addfa3e6246c85859612dd492259110345
Manish N <manish29na@gmail.com>
2023-03-28 20:47:55 +0530

    Add files via upload

---
+++ Coding Exercise/CE-11PlayingCat/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Boolean Result;
+        System.out.println(Result=PlayingCat.isCatPlaying(true,10));
+        System.out.println(Result=PlayingCat.isCatPlaying(false,36));
+        System.out.println(Result=PlayingCat.isCatPlaying(false,35));
+    }
+}
