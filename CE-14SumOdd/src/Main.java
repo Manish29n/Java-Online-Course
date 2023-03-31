ed7bf8b5298a1803d7bdc97dec1c7b3f81cda7b1
Manish N <manish29na@gmail.com>
2023-03-31 19:35:55 +0530

    Add files via upload

---
+++ CE-14SumOdd/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int Result;
+        System.out.println(Result=SumOdd.sumOdd(1,100));
+        System.out.println(Result=SumOdd.sumOdd(-1,100));
+        System.out.println(Result=SumOdd.sumOdd(100,100));
+        System.out.println(Result=SumOdd.sumOdd(13,13));
+        System.out.println(Result=SumOdd.sumOdd(100,-100));
+        System.out.println(Result=SumOdd.sumOdd(100,1000));
+    }
+}
