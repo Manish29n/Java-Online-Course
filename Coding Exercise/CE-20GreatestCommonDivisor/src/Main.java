c95918c98ea88a8ddcecefef00e625614094a4d8
Manish N <manish29na@gmail.com>
2023-04-04 20:52:40 +0530

    Add files via upload

---
+++ Coding Exercise/CE-20GreatestCommonDivisor/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(GreatestcommonDivisor.getGreatestCommonDivisor(25,15));
+        System.out.println(GreatestcommonDivisor.getGreatestCommonDivisor(12,30));
+        System.out.println(GreatestcommonDivisor.getGreatestCommonDivisor(9,18));
+        System.out.println(GreatestcommonDivisor.getGreatestCommonDivisor(81,153));
+    }
+}
